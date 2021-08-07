237550b6648e3f094fa4b5fe66c86a4948f1c8f8
rahulgupta13130 <rahulgupta13130@gmail.com>
2021-08-07 15:16:52 +0530

    changes in destructing

--- destructing.js
+++ destructing.js
@@ -7,3 +7,38 @@
 // let a,b;
 // [a,b,c,...d]=[10,20,30,40,50,60,70,80,90,100]
 // console.log(a,b,c,d)
+
+// object destructuring
+
+// let  bio={
+//     fname:"rahul",
+//     lname:"gupta",
+//     age:22,
+//     profession:'php developer'
+// }
+
+// let{fname,lname,age,profession}=bio;
+// console.log(profession);
+
+
+//-------------------------------------------
+
+
+
+// let  bio={
+//     fname:"rahul",
+//     lname:"gupta",
+//     age:22,
+//     profession:'php developer'
+// }
+// let fname,lname,age,profession;
+// ({fname,lname,age,profession}=bio);
+// console.log(profession);
+
+
+
+////===============================array destructuring 
+
+var lang=["php","html","css","js","mysql","bootstrap"]
+let [top1,top2,top3,top4,top5,top6]=lang;
+console.log(top5)
